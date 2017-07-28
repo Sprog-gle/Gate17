7c23aa6a31413b8a123dee051399d085847ae3e0
Andre Cowie <drecowie@gmail.com>
2017-07-28 23:45:10 +1200

    Some ux for the map

--- website/static/js/map.js
+++ website/static/js/map.js
@@ -5,9 +5,9 @@ var platform = new H.service.Platform({
   useHTTPS: true
 });
 
-function moveMapToBerlin(map){
-  map.setCenter({lat:-40.9006, lng:174.8860});
-  map.setZoom(4);
+function moveMapToNZ(map){
+  map.setCenter({lat:-41.29798905219789, lng:174.18283828125004});
+  map.setZoom(6);
 }
 
 var defaultLayers = platform.createDefaultLayers();
@@ -16,4 +16,4 @@ var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
 
 // Create the default UI components
 var ui = H.ui.UI.createDefault(map, defaultLayers);
-moveMapToBerlin(map);
+moveMapToNZ(map);
