170d3640218cde092ffdd1536f31f6eddd0e160a
Andre Cowie <drecowie@gmail.com>
2017-07-30 00:58:26 +1200

    New Data and some work on map

--- website/static/js/init.js
+++ website/static/js/init.js
@@ -1,8 +1,64 @@
+var tidalObjects = [];
+var tidalact = false;
+var rapidObjects = [];
+var rapidact = false;
+var swampObjects = [];
+var swampact = false;
+var residentialObjects = [];
+var residentialact = false;
+var map;
 (function($) {
     $(function() {
+        $("#lostuser").click(function() {
+            if ("geolocation" in navigator) {
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    console.log(position.coords.latitude+ " : "+ position.coords.longitude);
+                });
+            } else {
 
+            }
+        })
+        $("#rapidaction").click(function() {
+            if (rapidact) {
+                map.removeObjects(rapidObjects);
+                rapidact = false;
+            } else {
+                map.addObjects(rapidObjects);
+                rapidact = true;
+            }
+        });
+        $("#tidalaction").click(function() {
+            if (tidalact) {
+                map.removeObjects(tidalObjects);
+                tidalact = false;
+            } else {
+                map.addObjects(tidalObjects);
+                tidalact = true;
+            }
+        });
+        $("#residentialaction").click(function() {
+            if (residentialact) {
+                map.removeObjects(residentialObjects);
+                residentialact = false;
+            } else {
+                map.addObjects(residentialObjects);
+                residentialact = true;
+            }
+        })
+        $("#swampaction").click(function() {
+            if (swampact) {
+                map.removeObjects(swampObjects);
+                swampact = false;
+            } else {
+                map.addObjects(swampObjects);
+                swampact = true;
+            }
+        });
         $('.button-collapse').sideNav();
         $('.modal').modal();
+        $(".item-r").click(function() {
+            $("#lost").modal('open');
+        })
         $(".item-l").click(function() {
             $("#home").addClass("hide");
             $("#map").removeClass("hide");
@@ -22,66 +78,69 @@
             }
 
             var defaultLayers = platform.createDefaultLayers();
-            var map = new H.Map(document.getElementById('map'), defaultLayers.satellite.map);
+            map = new H.Map(document.getElementById('map'), defaultLayers.satellite.map);
             var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
 
             // Create the default UI components
             var ui = H.ui.UI.createDefault(map, defaultLayers);
             moveMapToNZ(map);
-            // $.getJSON("/swamps", function(data) {
-            //     var y;
-            //     for (x in data.swamps) {
-            //         var points = data.swamps[x][Object.keys(data.swamps[x])[0]];
-            //         var strip = new H.geo.Strip();
-            //         points.forEach(function(point) {
-            //             strip.pushPoint(point);
-            //         });
-            //         var polyline = new H.map.Polyline(strip, {
-            //             style: {
-            //                 lineWidth: 5,
-            //                 strokeColor: 'black'
-            //             }
-            //         });
-            //         map.addObject(polyline);
-            //     }
-            //     for (x in data.residentials) {
-            //         var points = data.residentials[x][Object.keys(data.residentials[x])[0]];
-            //         console.log(points);
-            //         var strip = new H.geo.Strip();
-            //         points.forEach(function(point) {
-            //             strip.pushPoint(point);
-            //         });
-            //         var polyline = new H.map.Polyline(strip, {
-            //             style: {
-            //                 lineWidth: 5,
-            //                 strokeColor: 'red'
-            //             }
-            //         });
-            //         map.addObject(polyline);
-            //     }
-            //     for (x in data.rapids) {
-            //         var points = data.rapids[x][Object.keys(data.rapids[x])[0]];
-            //         console.log(points);
-            //         var strip = new H.geo.Strip();
-            //         points.forEach(function(point) {
-            //             strip.pushPoint(point);
-            //         });
-            //         var polyline = new H.map.Polyline(strip, {
-            //             style: {
-            //                 lineWidth: 5,
-            //                 strokeColor: 'blue'
-            //             }
-            //         });
-            //         map.addObject(polyline);
-            //     }
-            // });
+            $.getJSON("/swamps", function(data) {
+                var y;
+                for (x in data.swamps) {
+                    var points = data.swamps[x][Object.keys(data.swamps[x])[0]];
+                    var strip = new H.geo.Strip();
+                    points.forEach(function(point) {
+                        strip.pushPoint(point);
+                    });
+                    var polyline = new H.map.Polyline(strip, {
+                        style: {
+                            lineWidth: 5,
+                            strokeColor: 'black'
+                        }
+                    });
+                    swampObjects.push(polyline);
+                }
+                for (x in data.residentials) {
+                    var points = data.residentials[x][Object.keys(data.residentials[x])[0]];
+                    console.log(points);
+                    var strip = new H.geo.Strip();
+                    points.forEach(function(point) {
+                        strip.pushPoint(point);
+                    });
+                    var polyline = new H.map.Polyline(strip, {
+                        style: {
+                            lineWidth: 5,
+                            strokeColor: 'red'
+                        }
+                    });
+                    residentialObjects.push(polyline);
+                }
+                for (x in data.rapids) {
+                    var points = data.rapids[x][Object.keys(data.rapids[x])[0]];
+                    console.log(points);
+                    var strip = new H.geo.Strip();
+                    points.forEach(function(point) {
+                        strip.pushPoint(point);
+                    });
+                    var polyline = new H.map.Polyline(strip, {
+                        style: {
+                            lineWidth: 5,
+                            strokeColor: 'blue'
+                        }
+                    });
+                    rapidObjects.push(polyline);
+                }
+            });
+
             $.getJSON("/tidal", function(data) {
                 for (x in data.tidals) {
-                  console.log(data.tidals[x]);
-                    var circle = new H.map.Circle(data.tidals[x], 800, {style: {fillColor: "yellow", strokeColor: "orange"}});
-
-                    // Add the circle to the map:
-                    map.addObject(circle);
+                    var circle = new H.map.Circle(data.tidals[x], 800, {
+                        style: {
+                            fillColor: "yellow",
+                            strokeColor: "orange"
+                        }
+                    });
+                    tidalObjects.push(circle);
                 }
             });
         });
