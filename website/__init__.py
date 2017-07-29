159c2091b13dfc2307306c42852cddba9db1c643
Andre Cowie <andre.cowie@airnz.co.nz>
2017-07-29 12:03:01 +1200

    Init py new endpoints

--- website/__init__.py
+++ website/__init__.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/projectraven')
+def projectRaven():
+    return render_template("projectraven.html")
+
+@app.route('/newLostPerson' methods=["POST"])
+def newLostPerson():
+    return "Success"
+    
 app.secret_key = 'f988d64c-e583-4368-8766-b6f1ba0e8305'
 
 if __name__ == "__main__":
