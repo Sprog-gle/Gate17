adec2b206e87815a5f66c26f313618602356a6ba
Oscar Cassetti <oscar.getstring@gmail.com>
2017-07-30 12:02:30 +1200

    Removing pynamo

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, url_for, session, redirect, jsonify
 import json
-from pynamodb import Model
 
 app = Flask(__name__)
 
@@ -43,23 +42,24 @@ def risk():
         risky = json.load(file)
     return jsonify(risky)
 
+
 @app.route("/lost")
-def risk():
-    with open("static/data/points-risk.json") as file:
-        risky = json.load(file)
-    return jsonify(risky)
+def lost():
+    with open("static/data/lost.json") as file:
+        obj = json.load(file)
+    return jsonify(obj)
 
 app.secret_key = 'f988d64c-e583-4368-8766-b6f1ba0e8305'
 
 if __name__ == "__main__":
     app.run()
 
-class Lost(Model):
-    class Meta:
-        table_name = "gate_17_lost"
-        region = "ap-southeast-2"
-
-    id = UnicodeAttribute(hash_key=True)
-    name = UnicodeAttribute()
-    type = UnicodeAttribute()
-    positions = UnicodeAttribute()
+# class Lost(Model):
+#     class Meta:
+#         table_name = "gate_17_lost"
+#         region = "ap-southeast-2"
+#
+#     id = UnicodeAttribute(hash_key=True)
+#     name = UnicodeAttribute()
+#     type = UnicodeAttribute()
+#     positions = UnicodeAttribute()
