4f9b40a7e8bd12d5e8d0baef6b29717dfcf08ff5
Lilmortal <jacktan165@gmail.com>
2017-07-30 12:49:08 +1200

    Close modal when submitted and show map

--- website/static/js/init.js
+++ website/static/js/init.js
@@ -44,8 +44,34 @@ var map;
     });
     $("#form-submit").click(function() {
       $("#lost-friend-form").addClass("hide");
-      $("#possible-location").removeClass("hide");
       $("#map-possible-location1").removeClass("hide");
+      $("#home").addClass("hide");
+      $("#lost").modal("close");
+      var platform = new H.service.Platform({
+        app_id: "BtRgpFTmtEuUrzEKGY7W",
+        app_code: "REnj-MMTZHpj29yC9JeFnw",
+        useCIT: true,
+        useHTTPS: true
+      });
+
+      function moveMapToNZ(map) {
+        map.setCenter({
+          lat: -41.29798905219789,
+          lng: 174.18283828125004
+        });
+        map.setZoom(6);
+      }
+
+      var defaultLayers = platform.createDefaultLayers();
+      map = new H.Map(
+        document.getElementById("map"),
+        defaultLayers.satellite.map
+      );
+      var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+
+      // Create the default UI components
+      var ui = H.ui.UI.createDefault(map, defaultLayers);
+      moveMapToNZ(map);
     });
 
     $("#lostuser").click(function() {
